8ebaab949a3687ea66d517422886ccbc77570427
Simeon <simeon.anfinrud@mac.com>
2019-03-15 22:08:43 -0700

    Added JSON serializing and deserializing.
    This will help save the todo list to a file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,130 @@
-type TaskId = i32;
+#[macro_use]
+extern crate json;
+
+use json::JsonValue;
+use std::convert::From;
+use std::slice::Iter;
+
+trait TryFrom<T>: Sized {
+    type Error;
+    fn try_from(value: T) -> Result<Self, Self::Error>;
+}
+
+type TaskId = usize;
+
+#[derive(Debug, PartialEq)]
+enum JsonType {
+    Array,
+    Dict,
+    // Num,
+    // Bool,
+    // Text,
+}
+
+#[derive(Debug, PartialEq)]
+enum FromJsonError {
+    MissingField(String),
+    WrongType(JsonType),
+}
+
+fn json_get_string(
+    json: &JsonValue,
+    field: &str,
+) -> Result<String, FromJsonError> {
+    match json[field].as_str() {
+        Some(value) => Ok(value.to_string()),
+        None => Err(FromJsonError::MissingField(field.to_string())),
+    }
+}
+
+fn json_get_array<'a>(
+    json: &'a JsonValue,
+    field: &str,
+) -> Result<Iter<'a, JsonValue>, FromJsonError> {
+    if !json.is_object() {
+        return Err(FromJsonError::WrongType(JsonType::Dict));
+    }
+    if !json.has_key(field) {
+        return Err(FromJsonError::MissingField(field.to_string()));
+    }
+    if !json[field].is_array() {
+        return Err(FromJsonError::WrongType(JsonType::Array));
+    }
+    Ok(json[field].members())
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Task {
-    pub description: String,
+    pub desc: String,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct TodoList {
-    next_id: TaskId,
-    tasks: Vec<(TaskId, Task)>,
+    tasks: Vec<Task>,
 }
 
 impl Task {
-    pub fn new(description: String) -> Task {
-        Task {
-            description: description,
-        }
+    pub fn new(desc: String) -> Task {
+        Task { desc: desc }
+    }
+}
+
+impl<'a> From<&'a Task> for JsonValue {
+    fn from(task: &'a Task) -> Self {
+        object!("desc" => *task.desc)
+    }
+}
+
+impl<'a> TryFrom<&'a JsonValue> for Task {
+    type Error = FromJsonError;
+    fn try_from(json: &'a JsonValue) -> Result<Self, Self::Error> {
+        Ok(Self {
+            desc: json_get_string(json, "desc")?,
+        })
     }
 }
 
 impl TodoList {
     pub fn new() -> TodoList {
-        TodoList {
-            next_id: 0,
-            tasks: Vec::new(),
-        }
+        TodoList { tasks: Vec::new() }
     }
 
     pub fn add(&mut self, task: Task) -> TaskId {
-        let id = self.next_id;
-        self.next_id += 1;
-        self.tasks.push((id, task));
-        id
+        self.tasks.push(task);
+        self.tasks.len() - 1
     }
 
     pub fn check(&mut self, id: TaskId) {
-        self.tasks.retain(|(task_id, _task)| *task_id != id);
+        self.tasks.remove(id);
     }
 
     pub fn incomplete_tasks(&self) -> impl Iterator<Item = &Task> {
-        self.tasks.iter().map(|(_id, task)| task)
+        self.tasks.iter()
     }
 }
+
+impl<'a> From<&'a TodoList> for JsonValue {
+    fn from(list: &'a TodoList) -> Self {
+        object!(
+            "tasks" => (
+                list.tasks.iter().map(JsonValue::from).collect::<Vec<_>>()
+            ),
+        )
+    }
+}
+
+impl<'a> TryFrom<&'a JsonValue> for TodoList {
+    type Error = FromJsonError;
+
+    fn try_from(json: &'a JsonValue) -> Result<Self, Self::Error> {
+        Ok(Self {
+            tasks: json_get_array(json, "tasks")?
+                .flat_map(Task::try_from)
+                .collect(),
+        })
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -127,4 +211,110 @@ mod tests {
         assert_eq!(tasks.next(), None);
     }
 
+    #[test]
+    fn empty_to_json() {
+        let list = TodoList::new();
+        assert_eq!(
+            JsonValue::from(&list),
+            object!(
+                "tasks" => array![],
+            )
+        );
+    }
+
+    #[test]
+    fn single_task_to_json() {
+        let mut list = TodoList::new();
+        list.add(Task::new("pass this test".to_string()));
+        assert_eq!(
+            JsonValue::from(&list),
+            object!(
+                "tasks" => array![
+                    object!("desc" => "pass this test")
+                ],
+            )
+        );
+    }
+
+    #[test]
+    fn three_tasks_to_json() {
+        let mut list = TodoList::new();
+        list.add(Task::new("first".to_string()));
+        list.add(Task::new("second".to_string()));
+        list.add(Task::new("third".to_string()));
+        assert_eq!(
+            JsonValue::from(&list),
+            object!(
+                "tasks" => array![
+                    object!("desc" => "first"),
+                    object!("desc" => "second"),
+                    object!("desc" => "third")
+                ],
+            )
+        );
+    }
+
+    #[test]
+    fn empty_from_json() {
+        let list = TodoList::new();
+        let json = object!(
+            "tasks" => array!(),
+        );
+        assert_eq!(TodoList::try_from(&json).unwrap(), list);
+    }
+
+    #[test]
+    fn single_task_from_json() {
+        let mut list = TodoList::new();
+        list.add(Task::new("check me out".to_string()));
+        let json = object!(
+            "tasks" => array!(
+                object!("desc" => "check me out")
+            ),
+        );
+        assert_eq!(TodoList::try_from(&json).unwrap(), list);
+    }
+
+    #[test]
+    fn three_tasks_from_json() {
+        let mut list = TodoList::new();
+        list.add(Task::new("three".to_string()));
+        list.add(Task::new("blind".to_string()));
+        list.add(Task::new("mice".to_string()));
+        let json = object!(
+            "tasks" => array!(
+                object!("desc" => "three"),
+                object!("desc" => "blind"),
+                object!("desc" => "mice")
+            ),
+        );
+        assert_eq!(TodoList::try_from(&json).unwrap(), list);
+    }
+
+    #[test]
+    fn todo_list_parse_fails_from_emtpy_object() {
+        let json = object!();
+        assert_eq!(
+            TodoList::try_from(&json),
+            Err(FromJsonError::MissingField("tasks".to_string()))
+        );
+    }
+
+    #[test]
+    fn todo_list_parse_fails_from_array() {
+        let json = array![];
+        assert_eq!(
+            TodoList::try_from(&json),
+            Err(FromJsonError::WrongType(JsonType::Dict))
+        );
+    }
+
+    #[test]
+    fn todo_list_parse_fails_missing_tasks_key() {
+        let json = object!("wrong_key" => "hi");
+        assert_eq!(
+            TodoList::try_from(&json),
+            Err(FromJsonError::MissingField("tasks".to_string()))
+        );
+    }
 }
