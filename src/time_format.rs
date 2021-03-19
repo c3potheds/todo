c9a50800fb8700944ea271b56ca3a3bfaa33880f
Simeon <simeon.anfinrud@mac.com>
2021-03-19 11:10:34 -0400

    Add relative time display for printing due dates.

--- src/time_format.rs
+++ src/time_format.rs
@@ -11,3 +11,30 @@ pub fn parse_time<Tz: TimeZone>(
         now + chrono::Duration::milliseconds(duration.as_millis() as i64)
     })
 }
+
+// The humantime::format_duration() function will format durations like "5m 32s"
+// to however much precision is representable. For "laconic" representation of
+// duration, presented to the user, we don't need second-level precision for
+// durations in the order of minutes, or minute-level precision for durations
+// in the order of hourse, etc, so we strip off all but the first "word" in the
+// formatted time.
+fn format_duration_laconic(duration: chrono::Duration) -> String {
+    let formatted =
+        humantime::format_duration(duration.to_std().unwrap().into());
+    match format!("{}", formatted).split(" ").next() {
+        Some(chunk) => chunk.to_string(),
+        None => panic!("Formatted duration is empty string: {}", formatted),
+    }
+}
+
+pub fn display_relative_time<Tz: TimeZone>(
+    now: DateTime<Tz>,
+    then: DateTime<Tz>,
+) -> String {
+    let duration = then - now;
+    if duration < chrono::Duration::zero() {
+        format!("{} ago", format_duration_laconic(-duration))
+    } else {
+        format!("in {}", format_duration_laconic(duration))
+    }
+}
--- src/time_format_test.rs
+++ src/time_format_test.rs
@@ -141,3 +141,75 @@ fn am_next_day() {
     let actual = parse_time(now, "5am").unwrap();
     assert_eq!(actual, expected);
 }
+
+#[test]
+fn relative_time_in_five_minutes() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 03, 19).and_hms(10, 05, 00);
+    let expected = "in 5m";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_minute_level_precision() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 03, 19).and_hms(10, 05, 30);
+    let expected = "in 5m";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_minute_level_precision_limit() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 03, 19).and_hms(10, 06, 59);
+    let expected = "in 6m";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_five_minutes_ago() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 03, 19).and_hms(09, 55, 00);
+    let expected = "5m ago";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_in_five_days() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 03, 24).and_hms(23, 59, 59);
+    let expected = "in 5days";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_in_eight_days() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 03, 27).and_hms(23, 59, 59);
+    let expected = "in 8days";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_in_a_month() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2021, 04, 27).and_hms(23, 59, 59);
+    let expected = "in 1month";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn relative_time_in_11_months() {
+    let now = Local.ymd(2021, 03, 19).and_hms(10, 00, 00);
+    let then = Local.ymd(2022, 03, 18).and_hms(23, 59, 59);
+    let expected = "in 11months";
+    let actual = display_relative_time(now, then);
+    assert_eq!(actual, expected);
+}
