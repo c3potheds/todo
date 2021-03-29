90625118cfac300baf1853df119be8c0961546a5
Simeon <simeon.anfinrud@mac.com>
2021-03-29 13:58:50 -0400

    Add tests for force-checking.
    These don't pass yet.

--- src/app/check_test.rs
+++ src/app/check_test.rs
@@ -4,6 +4,7 @@ use model::TodoList;
 use printing::Action;
 use printing::Expect;
 use printing::PrintableError;
+use printing::PrintableWarning;
 
 #[test]
 fn check_one_task() {
@@ -212,5 +213,109 @@ fn check_same_task_twice_in_one_command() {
             Expect::Status(TaskStatus::Complete),
             Expect::Action(Action::Check),
         ])
+        .end();
+}
+
+#[test]
+#[ignore = "todo-dev.app.check.force"]
+fn force_check_incomplete_task() {
+    let mut fix = Fixture::new();
+    fix.test("todo new a");
+    fix.test("todo check a --force")
+        .validate()
+        .printed_task(&[
+            Expect::Desc("a"),
+            Expect::Number(0),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .end();
+}
+
+#[test]
+#[ignore = "todo-dev.app.check.force"]
+fn force_check_blocked_task() {
+    let mut fix = Fixture::new();
+    fix.test("todo new a b --chain");
+    fix.test("todo check b --force")
+        .validate()
+        .printed_task(&[
+            Expect::Desc("a"),
+            Expect::Number(-1),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .printed_task(&[
+            Expect::Desc("b"),
+            Expect::Number(0),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .end();
+}
+
+#[test]
+#[ignore = "todo-dev.app.check.force"]
+fn force_check_transitively_blocked_task() {
+    let mut fix = Fixture::new();
+    fix.test("todo new a b c --chain");
+    fix.test("todo check c --force")
+        .validate()
+        .printed_task(&[
+            Expect::Desc("a"),
+            Expect::Number(-1),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .printed_task(&[
+            Expect::Desc("b"),
+            Expect::Number(0),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .printed_task(&[
+            Expect::Desc("c"),
+            Expect::Number(0),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .end();
+}
+
+#[test]
+#[ignore = "todo-dev.app.check.force"]
+fn force_check_task_with_complete_deps() {
+    let mut fix = Fixture::new();
+    fix.test("todo new a b");
+    fix.test("todo new c -p a b");
+    fix.test("todo check a");
+    fix.test("todo check c --force")
+        .validate()
+        .printed_task(&[
+            Expect::Desc("b"),
+            Expect::Number(0),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .printed_task(&[
+            Expect::Desc("c"),
+            Expect::Number(0),
+            Expect::Status(TaskStatus::Complete),
+            Expect::Action(Action::Check),
+        ])
+        .end();
+}
+
+#[test]
+#[ignore = "todo-dev.app.check.force"]
+fn force_check_complete_task() {
+    let mut fix = Fixture::new();
+    fix.test("todo new a");
+    fix.test("todo check a");
+    fix.test("todo check a --force")
+        .validate()
+        .printed_warning(&PrintableWarning::CannotCheckBecauseAlreadyComplete {
+            cannot_check: 1,
+        })
         .end();
 }
