13be4e3ab2e2fbc9e0fc19078fb91e003e25d0a1
Simeon <simeon.anfinrud@mac.com>
2021-04-23 10:36:12 -0400

    Add text editor to config.

--- src/config.rs
+++ src/config.rs
@@ -3,14 +3,20 @@ use std::io::Read;
 #[derive(Default, Debug, PartialEq, Deserialize, Serialize)]
 struct ConfigModel {
     paginator_cmd: Option<Vec<String>>,
+    text_editor_cmd: Option<String>,
 }
 
 pub struct Config {
     pub paginator_cmd: Vec<String>,
+    pub text_editor_cmd: String,
 }
 
 fn default_paginator_cmd() -> Vec<String> {
     vec!["less".to_string(), "-rX".to_string()]
+}
+
+fn default_text_editor_cmd() -> String {
+    "vim".to_string()
 }
 
 impl Config {
@@ -19,6 +25,9 @@ impl Config {
             paginator_cmd: model
                 .paginator_cmd
                 .unwrap_or_else(&default_paginator_cmd),
+            text_editor_cmd: model
+                .text_editor_cmd
+                .unwrap_or_else(&default_text_editor_cmd),
         }
     }
 }
@@ -27,6 +36,7 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             paginator_cmd: default_paginator_cmd(),
+            text_editor_cmd: default_text_editor_cmd(),
         }
     }
 }
