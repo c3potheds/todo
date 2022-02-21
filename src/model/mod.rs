c7665ecac2d6aee7c7f45ffbacb7808a35c2c133
Simeon <simeon.anfinrud@mac.com>
2022-02-21 11:40:35 -0500

    Extract TaskSet to dedicated module.

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -5,11 +5,9 @@ use chrono::Duration;
 use chrono::Utc;
 use daggy::stable_dag::StableDag;
 use daggy::Walker;
-use std::collections::BTreeSet;
 use std::collections::HashSet;
 use std::io::Read;
 use std::io::Write;
-use std::iter::FromIterator;
 
 mod task_id;
 pub use self::task_id::*;
@@ -26,140 +24,14 @@ pub use self::task::*;
 mod layering;
 use self::layering::*;
 
+mod task_set;
+pub use self::task_set::*;
+
 #[derive(Debug, Deserialize, Serialize, Default)]
 pub struct TodoList {
     tasks: StableDag<Task, ()>,
     complete: Vec<TaskId>,
     incomplete: Layering<TaskId>,
-}
-
-#[derive(Debug, PartialEq, Clone, Default)]
-pub struct TaskSet {
-    ids: HashSet<TaskId>,
-}
-
-#[derive(PartialEq, Eq)]
-struct TaskIdWithPosition {
-    position: i32,
-    id: TaskId,
-}
-
-impl PartialOrd for TaskIdWithPosition {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.position.partial_cmp(&other.position)
-    }
-}
-
-impl Ord for TaskIdWithPosition {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.position.cmp(&other.position)
-    }
-}
-
-impl TaskSet {
-    pub fn of(id: TaskId) -> Self {
-        TaskSet {
-            ids: HashSet::from_iter(std::iter::once(id)),
-        }
-    }
-
-    /// Iterates the set in an arbitrary order. Careful when using this; it may
-    /// cause non-determinism. It is more efficient than iterating in sorted
-    /// order.
-    pub fn iter_unsorted(&self) -> impl Iterator<Item = TaskId> + '_ {
-        self.ids.iter().copied()
-    }
-
-    pub fn into_iter_unsorted(self) -> impl Iterator<Item = TaskId> {
-        self.ids.into_iter()
-    }
-
-    pub fn contains(&self, id: TaskId) -> bool {
-        self.ids.contains(&id)
-    }
-
-    pub fn len(&self) -> usize {
-        self.ids.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.ids.is_empty()
-    }
-
-    /// Iterates the set in sorted order, where the ordering is defined by the
-    /// position in the list.
-    pub fn iter_sorted(&self, list: &TodoList) -> impl Iterator<Item = TaskId> {
-        self.ids
-            .iter()
-            .flat_map(|&id| {
-                list.position(id)
-                    .map(|pos| TaskIdWithPosition { id, position: pos })
-                    .into_iter()
-            })
-            .collect::<BTreeSet<_>>()
-            .into_iter()
-            .map(|task_id_with_pos| task_id_with_pos.id)
-    }
-
-    pub fn include_done(self, list: &TodoList, include_done: bool) -> Self {
-        if include_done {
-            self
-        } else {
-            TaskSet {
-                ids: HashSet::from_iter(self.ids.into_iter().filter(|&id| {
-                    list.status(id) != Some(TaskStatus::Complete)
-                })),
-            }
-        }
-    }
-}
-
-impl TaskSet {
-    pub fn product(
-        &self,
-        other: &Self,
-        list: &TodoList,
-    ) -> impl Iterator<Item = (TaskId, TaskId)> {
-        use itertools::Itertools;
-        self.iter_sorted(list).cartesian_product(
-            other.iter_sorted(list).collect::<Vec<_>>().into_iter(),
-        )
-    }
-}
-
-impl FromIterator<TaskId> for TaskSet {
-    fn from_iter<I: IntoIterator<Item = TaskId>>(iter: I) -> Self {
-        Self {
-            ids: iter.into_iter().collect(),
-        }
-    }
-}
-
-impl std::ops::BitOr for TaskSet {
-    type Output = TaskSet;
-    fn bitor(self, other: Self) -> Self::Output {
-        TaskSet {
-            ids: &self.ids | &other.ids,
-        }
-    }
-}
-
-impl std::ops::BitAnd for TaskSet {
-    type Output = TaskSet;
-    fn bitand(self, other: Self) -> Self::Output {
-        TaskSet {
-            ids: &self.ids & &other.ids,
-        }
-    }
-}
-
-impl std::ops::Sub<&TaskSet> for &TaskSet {
-    type Output = TaskSet;
-    fn sub(self, other: &TaskSet) -> Self::Output {
-        TaskSet {
-            ids: &self.ids - &other.ids,
-        }
-    }
 }
 
 impl TodoList {
@@ -490,7 +362,7 @@ impl TodoList {
                     ),
                 },
             );
-            result.unblocked.ids.remove(&options.id);
+            result.unblocked = &result.unblocked - &TaskSet::of(options.id);
             match self.check(options) {
                 Ok(newly_unblocked) => Ok(ForceChecked {
                     completed: result.completed
---
+++ src/model/task_set.rs
@@ -0,0 +1,135 @@
+use model::TaskId;
+use model::TaskStatus;
+use model::TodoList;
+use std::collections::BTreeSet;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+#[derive(Debug, PartialEq, Clone, Default)]
+pub struct TaskSet {
+    ids: HashSet<TaskId>,
+}
+
+#[derive(PartialEq, Eq)]
+struct TaskIdWithPosition {
+    position: i32,
+    id: TaskId,
+}
+
+impl PartialOrd for TaskIdWithPosition {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.position.partial_cmp(&other.position)
+    }
+}
+
+impl Ord for TaskIdWithPosition {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.position.cmp(&other.position)
+    }
+}
+
+impl TaskSet {
+    pub fn of(id: TaskId) -> Self {
+        TaskSet {
+            ids: HashSet::from_iter(std::iter::once(id)),
+        }
+    }
+
+    /// Iterates the set in an arbitrary order. Careful when using this; it may
+    /// cause non-determinism. It is more efficient than iterating in sorted
+    /// order.
+    pub fn iter_unsorted(&self) -> impl Iterator<Item = TaskId> + '_ {
+        self.ids.iter().copied()
+    }
+
+    pub fn into_iter_unsorted(self) -> impl Iterator<Item = TaskId> {
+        self.ids.into_iter()
+    }
+
+    pub fn contains(&self, id: TaskId) -> bool {
+        self.ids.contains(&id)
+    }
+
+    pub fn len(&self) -> usize {
+        self.ids.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.ids.is_empty()
+    }
+
+    /// Iterates the set in sorted order, where the ordering is defined by the
+    /// position in the list.
+    pub fn iter_sorted(&self, list: &TodoList) -> impl Iterator<Item = TaskId> {
+        self.ids
+            .iter()
+            .flat_map(|&id| {
+                list.position(id)
+                    .map(|pos| TaskIdWithPosition { id, position: pos })
+                    .into_iter()
+            })
+            .collect::<BTreeSet<_>>()
+            .into_iter()
+            .map(|task_id_with_pos| task_id_with_pos.id)
+    }
+
+    pub fn include_done(self, list: &TodoList, include_done: bool) -> Self {
+        if include_done {
+            self
+        } else {
+            TaskSet {
+                ids: HashSet::from_iter(self.ids.into_iter().filter(|&id| {
+                    list.status(id) != Some(TaskStatus::Complete)
+                })),
+            }
+        }
+    }
+}
+
+impl TaskSet {
+    pub fn product(
+        &self,
+        other: &Self,
+        list: &TodoList,
+    ) -> impl Iterator<Item = (TaskId, TaskId)> {
+        use itertools::Itertools;
+        self.iter_sorted(list).cartesian_product(
+            other.iter_sorted(list).collect::<Vec<_>>().into_iter(),
+        )
+    }
+}
+
+impl FromIterator<TaskId> for TaskSet {
+    fn from_iter<I: IntoIterator<Item = TaskId>>(iter: I) -> Self {
+        Self {
+            ids: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl std::ops::BitOr for TaskSet {
+    type Output = TaskSet;
+    fn bitor(self, other: Self) -> Self::Output {
+        TaskSet {
+            ids: &self.ids | &other.ids,
+        }
+    }
+}
+
+impl std::ops::BitAnd for TaskSet {
+    type Output = TaskSet;
+    fn bitand(self, other: Self) -> Self::Output {
+        TaskSet {
+            ids: &self.ids & &other.ids,
+        }
+    }
+}
+
+impl std::ops::Sub<&TaskSet> for &TaskSet {
+    type Output = TaskSet;
+    fn sub(self, other: &TaskSet) -> Self::Output {
+        TaskSet {
+            ids: &self.ids - &other.ids,
+        }
+    }
+}
