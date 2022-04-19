17d1725afbf3e43990ab1855c99d142b4438b0b8
Simeon <simeon.anfinrud@mac.com>
2022-04-19 16:38:08 -0400

    Extract main into crate.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,11 @@ extern crate chrono;
 extern crate itertools;
 extern crate structopt;
 extern crate textwrap;
-extern crate thiserror;
 
 extern crate clock;
 extern crate config;
-extern crate lookup_key;
 extern crate long_output;
+extern crate lookup_key;
 extern crate model;
 extern crate text_editing;
 extern crate time_format;
