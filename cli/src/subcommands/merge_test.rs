57608ba7a66e79ee24855ccfc2b5137226a63f0d
Simeon <simeon.anfinrud@mac.com>
2022-05-14 12:21:43 -0700

    Add --tag to 'merge' command parser.

--- cli/src/subcommands/merge_test.rs
+++ cli/src/subcommands/merge_test.rs
@@ -21,6 +21,7 @@ fn merge_one() {
         SubCommand::Merge(Merge {
             keys: vec![ByNumber(1)],
             into: "aa".to_string(),
+            ..Default::default()
         }),
     );
 }
@@ -32,6 +33,7 @@ fn merge_two() {
         SubCommand::Merge(Merge {
             keys: vec![ByNumber(1), ByNumber(2)],
             into: "ab".to_string(),
+            ..Default::default()
         }),
     );
 }
@@ -43,6 +45,55 @@ fn merge_three() {
         SubCommand::Merge(Merge {
             keys: vec![ByNumber(-1), ByNumber(-2), ByNumber(-3)],
             into: "abc".to_string(),
+            ..Default::default()
         }),
     );
+}
+
+#[test]
+fn merge_into_tag_true_long() {
+    expect_parses_into(
+        "todo merge 1 2 --into c --tag true",
+        SubCommand::Merge(Merge {
+            keys: vec![ByNumber(1), ByNumber(2)],
+            into: "c".to_string(),
+            tag: Some(true),
+        }),
+    )
+}
+
+#[test]
+fn merge_into_tag_false_long() {
+    expect_parses_into(
+        "todo merge 1 2 --into c --tag false",
+        SubCommand::Merge(Merge {
+            keys: vec![ByNumber(1), ByNumber(2)],
+            into: "c".to_string(),
+            tag: Some(false),
+        }),
+    )
+}
+
+#[test]
+fn merge_into_tag_true_short() {
+    expect_parses_into(
+        "todo merge 1 2 --into c -t true",
+        SubCommand::Merge(Merge {
+            keys: vec![ByNumber(1), ByNumber(2)],
+            into: "c".to_string(),
+            tag: Some(true),
+        }),
+    )
+}
+
+#[test]
+fn merge_into_tag_false_short() {
+    expect_parses_into(
+        "todo merge 1 2 --into c -t false",
+        SubCommand::Merge(Merge {
+            keys: vec![ByNumber(1), ByNumber(2)],
+            into: "c".to_string(),
+            tag: Some(false),
+        }),
+    )
 }
