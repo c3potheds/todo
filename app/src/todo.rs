cd2969b9b2c498d8c1bf1180aa0bec5fd419da91
c3potheds <simeon.anfinrud@mac.com>
2022-10-02 23:47:01 +0000

    Return app result from 'path' command.

--- app/src/todo.rs
+++ app/src/todo.rs
@@ -44,7 +44,7 @@ pub fn todo(
         Some(Log) => log::run(list).print(printer),
         Some(Merge(cmd)) => merge::run(list, now, &cmd).print(printer),
         Some(New(cmd)) => new::run(list, printer, now, &cmd),
-        Some(Path(cmd)) => path::run(list, printer, &cmd),
+        Some(Path(cmd)) => path::run(list, &cmd).print(printer),
         Some(Priority(cmd)) => priority::run(list, printer, &cmd),
         Some(Punt(cmd)) => punt::run(list, printer, &cmd),
         Some(Put(cmd)) => put::run(list, printer, &cmd),
