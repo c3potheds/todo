841015fa0e061633e3e12ec8a09315362036b348
c3potheds <simeon.anfinrud@mac.com>
2023-07-19 07:26:02 +0000

    Refactor printing tests.

--- printing/src/printable_result_test.rs
+++ printing/src/printable_result_test.rs
@@ -10,121 +10,82 @@ fn make_printable_task_with_number(number: i32) -> PrintableTask<'static> {
     }
 }
 
+fn success_with_task_numbers<const N: usize>(
+    numbers: [i32; N],
+) -> PrintableResult<'static> {
+    let tasks = numbers
+        .iter()
+        .map(|&number| make_printable_task_with_number(number))
+        .collect();
+    Ok(PrintableAppSuccess {
+        tasks,
+        ..Default::default()
+    })
+}
+
 #[test]
 fn max_index_digits_for_empty_list() {
-    let result: PrintableResult = Ok(Default::default());
+    let result = Ok(Default::default());
     assert_eq!(result.max_index_digits(), 1);
 }
 
 #[test]
 fn max_index_digits_for_list_of_one_single_digit_number() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![make_printable_task_with_number(1)],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([1]);
     assert_eq!(result.max_index_digits(), 1);
 }
 
 #[test]
 fn max_index_digits_for_list_of_single_digit_numbers() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![
-            make_printable_task_with_number(1),
-            make_printable_task_with_number(2),
-            make_printable_task_with_number(3),
-        ],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([1, 2, 3]);
     assert_eq!(result.max_index_digits(), 1);
 }
 
 #[test]
 fn max_index_digits_for_task_at_index_zero() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![make_printable_task_with_number(0)],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([0]);
     assert_eq!(result.max_index_digits(), 1);
 }
 
 #[test]
 fn max_index_digits_for_task_with_negative_number() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![make_printable_task_with_number(-1)],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([-1]);
     assert_eq!(result.max_index_digits(), 2);
 }
 
 #[test]
 fn max_index_digits_for_task_with_three_digit_number() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![make_printable_task_with_number(100)],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([100]);
     assert_eq!(result.max_index_digits(), 3);
 }
 
 #[test]
 fn max_index_digits_for_task_with_negative_three_digit_number() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![make_printable_task_with_number(-100)],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([-100]);
     assert_eq!(result.max_index_digits(), 4);
 }
 
 #[test]
 fn max_index_digits_for_list_of_three_digit_numbers() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![
-            make_printable_task_with_number(1),
-            make_printable_task_with_number(20),
-            make_printable_task_with_number(300),
-        ],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([1, 20, 300]);
     assert_eq!(result.max_index_digits(), 3);
 }
 
 #[test]
 fn max_index_digits_for_list_of_negative_three_digit_numbers() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![
-            make_printable_task_with_number(-1),
-            make_printable_task_with_number(-20),
-            make_printable_task_with_number(-300),
-        ],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([-1, -20, -300]);
     assert_eq!(result.max_index_digits(), 4);
 }
 
 #[test]
 fn max_index_digits_for_list_of_mixed_digit_numbers() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![
-            make_printable_task_with_number(-1),
-            make_printable_task_with_number(20),
-            make_printable_task_with_number(-300),
-        ],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([-1, 20, -300]);
     assert_eq!(result.max_index_digits(), 4);
 }
 
 #[test]
 fn max_index_digits_for_list_containing_negative_five_digit_numbers() {
-    let result: PrintableResult = Ok(PrintableAppSuccess {
-        tasks: vec![
-            make_printable_task_with_number(-1),
-            make_printable_task_with_number(20),
-            make_printable_task_with_number(-300),
-            make_printable_task_with_number(4000),
-            make_printable_task_with_number(-50000),
-        ],
-        ..Default::default()
-    });
+    let result = success_with_task_numbers([-10000, 20000, -30000]);
     assert_eq!(result.max_index_digits(), 6);
 }
 
