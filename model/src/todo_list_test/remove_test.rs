80bf8e05c490bd1400cbbeaf32fad4f1fa87b4a9
Simeon <simeon.anfinrud@mac.com>
2023-09-29 12:07:55 -0700

    Explicitly unblock neighbors of a removed task.
    When removing a node from the graph that has edges, the neighbors
    on the other side of he edge will not update their implicits, so
    their priorities, due dates, and tag values can be stale.
    
    The most egregious symptom would occur when removing a tag, whose
    TaskId could be recycled for a new task later. If any dependencies
    of that tag had that task's id in their implicit_tags list, even
    if the new task assigned to that id is not a tag, the dependency
    would render the new task's full description as an implicit tag.
    
    For example:
    
      todo new a b
      todo tag b
      todo rm b
      todo new c
      todo get a
      >> 1) <tag>c</tag> a
    
    By unblocking adeps from a removed task and unblocking the removed
    task from its deps before removing the task's node, the implicits
    will be ensured to be up-to-date.
    
    This changes the output of 'todo rm' when removing tasks that have
    deps. The list of tasks shown will now include all tasks affected
    by manipulating the neighbors of the removed tasks, which at minimum
    will include all immediate deps.

--- model/src/todo_list_test/remove_test.rs
+++ model/src/todo_list_test/remove_test.rs
@@ -35,10 +35,10 @@ fn remove_task_attaches_deps_to_adeps() {
     let c = list.add("c");
     list.block(b).on(a).unwrap();
     list.block(c).on(b).unwrap();
-    let adeps = list.remove(b);
+    let affected = list.remove(b);
     assert_eq!(list.all_tasks().collect::<Vec<_>>(), [a, c]);
     assert_eq!(list.status(c), Some(TaskStatus::Blocked));
-    assert_eq!(adeps.as_sorted_vec(&list), [c]);
+    assert_eq!(affected.as_sorted_vec(&list), [a, c]);
 }
 
 #[test]
@@ -54,11 +54,46 @@ fn remove_task_attaches_all_deps_to_adeps() {
     list.block(c).on(b).unwrap();
     list.block(d).on(c).unwrap();
     list.block(e).on(c).unwrap();
-    let adeps = list.remove(c);
+    let affected = list.remove(c);
     assert_eq!(list.all_tasks().collect::<Vec<_>>(), [a, b, d, e]);
     assert_eq!(list.status(a), Some(TaskStatus::Incomplete));
     assert_eq!(list.status(b), Some(TaskStatus::Incomplete));
     assert_eq!(list.status(d), Some(TaskStatus::Blocked));
     assert_eq!(list.status(e), Some(TaskStatus::Blocked));
-    assert_eq!(adeps.as_sorted_vec(&list), [d, e]);
+    assert_eq!(affected.as_sorted_vec(&list), [a, b, d, e]);
+}
+
+#[test]
+fn implicit_priority_is_cleaned_up() {
+    let mut list = TodoList::default();
+    let a = list.add("a");
+    let b = list.add(NewOptions::new().desc("b").priority(1));
+    list.block(b).on(a).unwrap();
+    assert_eq!(list.get(a).unwrap().implicit_priority, 1);
+    list.remove(b);
+    assert_eq!(list.get(a).unwrap().implicit_priority, 0);
+}
+
+#[test]
+fn implicit_due_date_is_cleaned_up() {
+    use testing::ymdhms;
+    let mut list = TodoList::default();
+    let a = list.add("a");
+    let b_due = ymdhms(2020, 1, 1, 0, 0, 0);
+    let b = list.add(NewOptions::new().desc("b").due_date(b_due.clone()));
+    list.block(b).on(a).unwrap();
+    assert_eq!(list.get(a).unwrap().implicit_due_date, Some(b_due.clone()));
+    list.remove(b);
+    assert_eq!(list.get(a).unwrap().implicit_due_date, None);
+}
+
+#[test]
+fn implicit_tags_are_cleaned_up() {
+    let mut list = TodoList::default();
+    let a = list.add("a");
+    let b = list.add(NewOptions::new().desc("b").as_tag());
+    list.block(b).on(a).unwrap();
+    assert_eq!(list.get(a).unwrap().implicit_tags, vec![b]);
+    list.remove(b);
+    assert_eq!(list.get(a).unwrap().implicit_tags, vec![]);
 }
